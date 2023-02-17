95bd6ef4af262cf759a850e7b8d7efc536e5fcb5
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2023-02-16 21:17:19 -0700

    update dist folder, final commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,15 +12,15 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   console.log('Post to the database');
 
   const jateDB = await openDB("jate", 1);
 
-  const tx = jateDB.transaction("jate", "readwrite");
+  const txt = jateDB.transaction("jate", "readwrite");
 
-  const store = txt.createObjectStore("jate");
+  const store = txt.objectStore("jate");
 
   const request = store.put({ id: 1, value: content});
 
@@ -28,15 +28,15 @@ export const putDb = async (content) => {
     console.log("data saved to database!", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   console.log('GET from the database');
 
   const jateDB = await openDB("jate", 1);
 
-  const tx = jateDB.transaction("jate", "readonly");
+  const txt = jateDB.transaction("jate", "readonly");
 
-  const store = txt.createObjectStore("jate");
+  const store = txt.objectStore("jate");
 
   const request = store.getAll();
 
