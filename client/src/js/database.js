a19500e8592b79ce6be1ad0a9d5a21d1ac1dfca2
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2023-02-16 18:36:22 -0700

    add database functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the database');
+
+  const jateDB = await openDB("jate", 1);
+
+  const tx = jateDB.transaction("jate", "readwrite");
+
+  const store = txt.createObjectStore("jate");
+
+  const request = store.put({ id: 1, value: content});
+
+  const result = await request;
+    console.log("data saved to database!", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const jateDB = await openDB("jate", 1);
+
+  const tx = jateDB.transaction("jate", "readonly");
+
+  const store = txt.createObjectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log("result.value", result.value);
+  return result.value;
+};
 
 initdb();
